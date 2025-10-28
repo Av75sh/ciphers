848bb71fe9fc32a83f05616f4f496ab6bb898444
Av75sh <avinashlpu75@gmail.com>
2025-10-29 01:26:30 +0530

    render

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: process.env.NODE_ENV === 'production' 
+    ? ['https://ciphers-five.vercel.app/']
+    : ['http://localhost:5173'],
   credentials: true
 }));
 
